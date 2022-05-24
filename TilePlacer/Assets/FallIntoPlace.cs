c1e440d1046bd5ab860eb84aba7271186048e832
551919 <551919@my.cuhsd.org>
2022-05-24 15:14:45 -0700

    Update FallIntoPlace.cs

--- TilePlacer/Assets/FallIntoPlace.cs
+++ TilePlacer/Assets/FallIntoPlace.cs
@@ -18,12 +18,11 @@ public class FallIntoPlace : MonoBehaviour
     public float BlockFallLength;
 
 
-
     private Vector3 velocity = Vector3.zero;
 
     private float elapsedTime = 0;
 
-    public int fallAmount = 5;
+    public int fallAmount = 6;
 
     private int t = 1;
 
@@ -44,14 +43,14 @@ public class FallIntoPlace : MonoBehaviour
         //iterate through every block and log starting Y position to the BlockHeights array
         for (int i = 1; i < Blocks.Length; i++)
         {
-            BlockHeights[i] = Blocks[i].transform.position.y;
+            BlockHeights[i] = Blocks[i].transform.localPosition.y;
             Blocks[i].gameObject.SetActive(false);
         }
 
         //Setup first cube for falling
 
         BlockFall.Add(Blocks[1].gameObject);
-        BlockFall[0].SetActive(true);     
+        BlockFall[0].SetActive(true);
     }
     
     // Update is called once per frame
@@ -61,7 +60,7 @@ public class FallIntoPlace : MonoBehaviour
 
         if (Input.GetMouseButton(0) && Input.mousePosition.y > 400)
         {
-            GetBlockList();
+            //GetBlockList();
             MoveBlock();
         }
         else
@@ -73,7 +72,7 @@ public class FallIntoPlace : MonoBehaviour
 
     private void GetBlockList()
     {
-        if (Blocks[t].transform.position.y <= BlockHeights[t]-fallAmount/2)
+        if (BlockFall[t-1].transform.position.y <= BlockHeights[t]-(fallAmount/2))
         {       
             t += 1;
             BlockFall.Add(Blocks[t].gameObject);
@@ -86,13 +85,23 @@ public class FallIntoPlace : MonoBehaviour
     {
         for (int i = 0; i < BlockFall.Count; i++)
         {
-            if (BlockFall[i].transform.position.y <= BlockHeights[i + 1]-fallAmount)
+            BlockFall[i].transform.Translate(5 * Time.deltaTime * Vector3.back);
+            if (BlockFall[i].transform.localPosition.y <= BlockHeights[i + 1]-fallAmount)
             {
-                BlockFall[i].transform.position = new Vector3(0,BlockHeights[i+1]-fallAmount,0);
+                BlockFall[i].transform.localPosition = new Vector3(BlockFall[i].transform.localPosition.x,BlockHeights[i+1]-fallAmount, BlockFall[i].transform.localPosition.z);
                 BlockFall.RemoveAt(i);
 
             }
-            BlockFall[i].transform.Translate(30 * Time.deltaTime * Vector3.back);
+            
+        }
+
+        
+        if(BlockFall[BlockFall.Count-1].transform.localPosition.y <= BlockHeights[BlockFall.Count-1]-(fallAmount/2))
+        {
+            BlockFall.Add(Blocks[BlockFall.Count].gameObject);
+            BlockFall[BlockFall.Count-1].SetActive(true);
+            //t += 1;
+            Debug.Log("troll");
         }
         
     }
